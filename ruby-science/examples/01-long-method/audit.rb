e644ded8e5ddc0ef5c721c9aac953bc614fb1d1e
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-02-24 19:19:30 +0300

    split long method

--- ruby-science/examples/01-long-method/audit.rb
+++ ruby-science/examples/01-long-method/audit.rb
@@ -1,14 +1,31 @@
 class Audit
   def archive!
     self.status = 'archive'
-    if save
-      todos.destroy_all
-      destroy_old_attachment_versions
-      attached_documents.reload.each(&:unlock!)
-      objectives.each(&:release_lock!)
-      project_roles.includes(:relationship).each do |r|
-        r.update_attributes(:name => 'reviewer')
-      end
+
+    save && clear_audit
+  end
+
+  private
+
+  def clear_audit
+    destroy_associated
+    unlock_associated!
+    update_project_roles
+  end
+
+  def destroy_associated
+    todos.destroy_all
+    destroy_old_attachment_versions
+  end
+
+  def unlock_associated!
+    attached_documents.reload.each(&:unlock!)
+    objectives.each(&:release_lock!)
+  end
+
+  def update_project_roles
+    project_roles.includes(:relationship).each do |project_role|
+      project_role.update_attributes(name: 'reviewer')
     end
   end
 end
