c1e7f51c64937bc0d0d26bae9ff70ebd0eb6dfc2
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-03-01 12:23:45 +0300

    extract CarwashPolicy class

--- ruby-science/examples/05-shotgun-surgery/clients_controller.rb
+++ ruby-science/examples/05-shotgun-surgery/clients_controller.rb
@@ -1,29 +1,50 @@
 class ClientsController
    def create
     @client = @carwash.clients.new(client_params)
-    
-    if current_user.is_owner_of?(@carwash) || current_user.is_manager?
-      @client.bonus = params[:client][:bonus] if params[:client][:bonus].present?
-      @client.status = params[:client][:status]
-    end
+    set_bonus_and_state
 
     if @client.save
-      redirect_to @client, notice: 'Клиент успешно добавлен' }
+      redirect_to @client, notice: 'Клиент успешно добавлен'
     else
       render action: 'new'
     end
   end
 
   def update
-    if current_user.is_owner_of?(@carwash) || current_user.is_manager?
-      @client.bonus = params[:client][:bonus] if params[:client][:bonus].present?
-      @client.status = params[:client][:status]
-    end
+    set_bonus_and_state
 
     if @client.update_attributes(client_params)
       redirect_to @client, notice: 'Данные клиента сохранены'
     else
       render action: 'edit'
     end
+  end
+
+  private
+
+  def set_bonus_and_state
+    return unless CarwashPolicy.new(current_user, @carwash).manage?
+
+    @client.set_bonus_and_state(params[:client])
+  end
+end
+
+class CarwashPolicy
+  attr_reader :user, :carwash
+
+  def initialize(user, carwash)
+    @user = user
+    @carwash = carwash
+  end
+
+  def manage?
+    user.is_owner_of?(carwash) || user.is_manager?
+  end
+end
+
+class Client < ApplicationRecord
+  def set_bonus_and_state(params)
+    self.bonus = params[:bonus] if params[:bonus].present?
+    self.status = params[:status]
   end
 end
