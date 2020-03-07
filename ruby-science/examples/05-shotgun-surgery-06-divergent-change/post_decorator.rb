00d0ab9aa5afceb23a993e0b9948f31420685cc3
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-03-07 13:38:50 +0300

    extract decorators for Post and Comment

---
+++ ruby-science/examples/05-shotgun-surgery-06-divergent-change/post_decorator.rb
@@ -0,0 +1,9 @@
+class PostDecorator < ApplicationDecorator
+  delegate :title, :body
+
+  decorates_association :comments
+
+  def date(format:)
+    DateTime.new(object.year.to_i, object.month.to_i, object.day.to_i).to_s(format)
+  end
+end
