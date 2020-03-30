61c8d228d2968f916194dd284af2760764970167
Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>
2020-03-30 23:54:18 +0300

    add partials

--- ruby-science/examples/05-shotgun-surgery-06-divergent-change/post_decorator.rb
+++ ruby-science/examples/05-shotgun-surgery-06-divergent-change/post_decorator.rb
@@ -6,4 +6,8 @@ class PostDecorator < ApplicationDecorator
   def date(format:)
     DateTime.new(object.year.to_i, object.month.to_i, object.day.to_i).to_s(format)
   end
+
+  def comments_are_present?
+    comments.size.positive?
+  end
 end
